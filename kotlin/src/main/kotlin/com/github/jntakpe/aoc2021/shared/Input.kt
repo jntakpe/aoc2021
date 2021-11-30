51017f97b85736d7a36f7f1fe1e973edc7f77df5
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-11-30 21:57:49 +0100

    init Kotlin project

---
+++ kotlin/src/main/kotlin/com/github/jntakpe/aoc2021/shared/Input.kt
@@ -0,0 +1,11 @@
+package com.github.jntakpe.aoc2021.shared
+
+fun readInputLines(day: Int) = readInput(day).lines()
+
+fun readInputNumbers(day: Int) = readInputLines(day).map { it.toInt() }
+
+fun readInputSplitOnBlank(day: Int) = readInput(day).split("(?m)^\\s*$".toRegex()).map { it.trim() }
+
+private fun readInput(day: Int) = "/day_$day.txt".run { resource()?.readText()?.trim() ?: error("Unable to read file: '$this'") }
+
+private fun String.resource() = object {}::class.java.getResource(this)
