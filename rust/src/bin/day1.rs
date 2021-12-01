f68e6fb4e6ccbe1ee11771d8270e8730474fa469
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-01 09:37:27 +0100

    first day rust implementation

--- rust/src/bin/day1.rs
+++ rust/src/bin/day1.rs
@@ -1,20 +1,23 @@
-use aoc_2021::shared::Day;
+use aoc_2021::shared::{Day, read_input_numbers};
 
 fn main() {
-    todo!()
+    Day1 {
+        input: read_input_numbers(1)
+    }.run()
 }
 
 struct Day1 {
-    input: Vec<String>
+    input: Vec<usize>,
 }
 
 impl Day for Day1 {
     fn part1(&self) -> usize {
-        todo!()
+        self.input.windows(2).filter(|p| p[0] < p[1]).count()
     }
 
     fn part2(&self) -> usize {
-        todo!()
+        let sums: Vec<usize> = self.input.windows(3).map(|w| w.iter().sum()).collect();
+        sums.windows(2).filter(|p| p[0] < p[1]).count()
     }
 }
 
@@ -24,17 +27,17 @@ mod tests {
 
     #[test]
     fn part1_sample() {
-        assert_eq!(sample_day().part1(), 0)
+        assert_eq!(sample_day().part1(), 7)
     }
 
     #[test]
     fn part2_sample() {
-        assert_eq!(sample_day().part2(), 0)
+        assert_eq!(sample_day().part2(), 5)
     }
 
     fn sample_day() -> Day1 {
         Day1 {
-            input: vec![String::from("foo")]
+            input: vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
         }
     }
 }
