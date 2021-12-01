adf872b159c68fe450e044bf4a0ba100934d2fe1
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-01 09:03:06 +0100

    first day implementation

--- kotlin/src/main/kotlin/com/github/jntakpe/aoc2021/AdventOfCode2021.kt
+++ kotlin/src/main/kotlin/com/github/jntakpe/aoc2021/AdventOfCode2021.kt
@@ -1,8 +1,10 @@
 package com.github.jntakpe.aoc2021
 
+import com.github.jntakpe.aoc2021.days.day1.Day1
+
 fun main(args: Array<String>) {
     when (val day = args[0].toInt()) {
-        1 -> TODO()
+        1 -> Day1.run()
         else -> error("Day $day not implemented yet")
     }
 }
---
+++ kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/days/day1/Day1Test.kt
@@ -0,0 +1,17 @@
+package com.github.jntakpe.aoc2021.days.day1
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day1Test {
+
+    @Test
+    fun `part 1 sample is ok`() {
+        assertEquals(7, Day1.part1())
+    }
+
+    @Test
+    fun `part 2 sample is ok`() {
+        assertEquals(5, Day1.part2())
+    }
+}
