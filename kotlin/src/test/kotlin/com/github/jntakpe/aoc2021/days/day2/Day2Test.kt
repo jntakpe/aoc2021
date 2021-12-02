d1d543c0cdf3a154819584492990180ee27964ad
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-02 09:05:54 +0100

    second day kotlin implementation

---
+++ kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/days/day2/Day2Test.kt
@@ -0,0 +1,17 @@
+package com.github.jntakpe.aoc2021.days.day2
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day2Test {
+
+    @Test
+    fun `part 1 sample is ok`() {
+        assertEquals(150, Day2.part1())
+    }
+
+    @Test
+    fun `part 2 sample is ok`() {
+        assertEquals(900, Day2.part2())
+    }
+}
