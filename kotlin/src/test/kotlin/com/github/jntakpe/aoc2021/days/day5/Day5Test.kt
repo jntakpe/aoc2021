cd58274ba69a7619c1a42cffbcb905351a0451a0
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-05 21:03:59 +0100

    implement advent of code day 5 in kotlin

---
+++ kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/days/day5/Day5Test.kt
@@ -0,0 +1,10 @@
+package com.github.jntakpe.aoc2021.days.day5
+
+import com.github.jntakpe.aoc2021.shared.DayTest
+
+class Day5Test : DayTest<Day5>() {
+
+    override val day: Day5 = Day5
+    override val expectPart1 = 5
+    override val expectPart2 = 12
+}
