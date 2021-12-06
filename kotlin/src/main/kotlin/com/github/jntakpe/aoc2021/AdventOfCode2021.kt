2f03e8a5ca3d5facfc61ab25ca716a509e49d7d0
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-06 09:58:00 +0200

    implement kotlin day 6

--- kotlin/src/main/kotlin/com/github/jntakpe/aoc2021/AdventOfCode2021.kt
+++ kotlin/src/main/kotlin/com/github/jntakpe/aoc2021/AdventOfCode2021.kt
@@ -5,6 +5,7 @@ import com.github.jntakpe.aoc2021.days.day2.Day2
 import com.github.jntakpe.aoc2021.days.day3.Day3
 import com.github.jntakpe.aoc2021.days.day4.Day4
 import com.github.jntakpe.aoc2021.days.day5.Day5
+import com.github.jntakpe.aoc2021.days.day6.Day6
 
 fun main(args: Array<String>) {
     when (val day = args[0].toInt()) {
@@ -13,6 +14,7 @@ fun main(args: Array<String>) {
         3 -> Day3.run()
         4 -> Day4.run()
         5 -> Day5.run()
+        6 -> Day6.run()
         else -> error("Day $day not implemented yet")
     }
 }
---
+++ kotlin/src/main/kotlin/com/github/jntakpe/aoc2021/days/day6/Day6.kt
@@ -0,0 +1,24 @@
+package com.github.jntakpe.aoc2021.days.day6
+
+import com.github.jntakpe.aoc2021.shared.Day
+import com.github.jntakpe.aoc2021.shared.readInputLines
+
+object Day6 : Day {
+
+    override val input =
+        readInputLines(6).joinToString("").split(",").groupingBy { it.trim().toInt() }.eachCount().mapValues { it.value.toLong() }
+
+    override fun part1() = input.total(80)
+
+    override fun part2() = input.total(256)
+
+    private fun Map<Int, Long>.total(remaining: Int): Long {
+        val newborn = get(0) ?: 0
+        val updated = filter { it.key != 0 }.mapKeys { it.key - 1 }.toMutableMap()
+        if (newborn > 0) {
+            updated[8] = newborn
+            updated[6] = newborn + updated.getOrDefault(6, 0)
+        }
+        return if (remaining == 1) updated.map { it.value }.sum() else updated.total(remaining - 1)
+    }
+}
