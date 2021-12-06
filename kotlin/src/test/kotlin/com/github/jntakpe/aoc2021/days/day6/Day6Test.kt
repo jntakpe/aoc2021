5fcac4cffdce727c7cbd931091be16ba99fd80c8
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-06 09:58:00 +0200

    implement kotlin day 6

---
+++ kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/days/day6/Day6Test.kt
@@ -0,0 +1,10 @@
+package com.github.jntakpe.aoc2021.days.day6
+
+import com.github.jntakpe.aoc2021.shared.DayTest
+
+internal class Day6Test : DayTest<Day6>() {
+
+    override val day = Day6
+    override val expectPart1 = 5934L
+    override val expectPart2 = 26984457539L
+}
