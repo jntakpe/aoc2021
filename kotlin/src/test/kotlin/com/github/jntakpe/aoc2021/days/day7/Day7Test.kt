becfd0034183075ebf8a435e48e7be0f2a4d9a0d
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-07 08:42:25 +0100

    day 7 kotlin implementation

---
+++ kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/days/day7/Day7Test.kt
@@ -0,0 +1,10 @@
+package com.github.jntakpe.aoc2021.days.day7
+
+import com.github.jntakpe.aoc2021.shared.DayTest
+
+internal class Day7Test : DayTest<Day7>() {
+
+    override val day = Day7
+    override val expectPart1 = 37
+    override val expectPart2 = 168
+}
