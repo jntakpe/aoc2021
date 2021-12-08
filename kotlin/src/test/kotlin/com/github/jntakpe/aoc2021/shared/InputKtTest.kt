335cec426d0fd952db8800d9e8be18a8d342281f
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-08 09:45:00 +0200

    make readInput method public

--- kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/shared/InputKtTest.kt
+++ kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/shared/InputKtTest.kt
@@ -48,7 +48,7 @@ internal class InputKtTest {
             assertEquals(4, lines.size)
             assertEquals(listOf(
                 """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
-byr:1937 iyr:2017 cid:147 hgt:183cm""".trimIndent(),
+byr:1937 iyr:2017 cid:147 hgt:183cm""",
                 """iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
 hcl:#cfa07d byr:1929""",
                 """hcl:#ae17e1 iyr:2013
@@ -57,6 +57,23 @@ ecl:brn pid:760753108 byr:1931
 hgt:179cm""",
                 """hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in"""), lines)
+        }
+    }
+
+    @Nested
+    inner class ReadInput {
+
+        @Test
+        fun `should read raw input`() {
+            assertEquals(
+                """
+                forward 5
+                down 5
+                forward 8
+                up 3
+                down 8
+                forward 2
+                """.trimIndent(), readInput(2))
         }
     }
 }
