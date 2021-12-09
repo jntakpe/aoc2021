ba0287b0e64b816eb69e8e72056402a635ea40ef
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-09 10:11:00 +0200

    kotlin day 9 implementation

---
+++ kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/days/day9/Day9Test.kt
@@ -0,0 +1,10 @@
+package com.github.jntakpe.aoc2021.days.day9
+
+import com.github.jntakpe.aoc2021.shared.DayTest
+
+internal class Day9Test : DayTest<Day9>() {
+
+    override val day = Day9
+    override val expectPart1 = 15
+    override val expectPart2 = 1134
+}
