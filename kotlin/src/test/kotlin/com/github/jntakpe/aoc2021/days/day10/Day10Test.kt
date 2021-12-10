6f239ca8cd02bd84cb290a20ba18c4515ac058f2
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-10 09:50:00 +0200

    implement day 10 in kotlin

---
+++ kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/days/day10/Day10Test.kt
@@ -0,0 +1,10 @@
+package com.github.jntakpe.aoc2021.days.day10
+
+import com.github.jntakpe.aoc2021.shared.DayTest
+
+class Day10Test : DayTest<Day10>() {
+
+    override val day = Day10
+    override val expectPart1 = 26397
+    override val expectPart2 = 288957L
+}
