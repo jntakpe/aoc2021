4d9d8824d48480dd089c963a8336cc190c037b90
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-12-12 21:32:00 +0200

    implement Kotlin day 12

---
+++ kotlin/src/test/kotlin/com/github/jntakpe/aoc2021/days/day12/Day12Test.kt
@@ -0,0 +1,10 @@
+package com.github.jntakpe.aoc2021.days.day12
+
+import com.github.jntakpe.aoc2021.shared.DayTest
+
+internal class Day12Test : DayTest<Day12>() {
+
+    override val day = Day12
+    override val expectPart1 = 226
+    override val expectPart2 = 3509
+}
